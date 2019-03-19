b617de60bd95972c86ad5ed33a240b743803e526
Amy Kirasack <amykirasack@gmail.com>
2019-03-19 13:10:40 -0400

    build: Create recipes schema

---
+++ lib/eggs_are_cool/schema/recipe.ex
@@ -0,0 +1,9 @@
+defmodule EggsAreCool.Schema.Recipe do
+  use Ecto.Schema
+  
+  schema "recipes" do
+    field :name, :string
+    field :description, :string
+    field :difficulty, :integer
+  end
+end
