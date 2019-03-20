95b535cd5ca848351de553ad7745ed2c4239a32c
Amy Kirasack <amykirasack@gmail.com>
2019-03-19 23:42:48 -0400

    style: Format, add observations

--- priv/repo/migrations/20190307161342_add_posts_table.exs
+++ priv/repo/migrations/20190307161342_add_posts_table.exs
@@ -2,9 +2,11 @@ defmodule EggsAreCool.Repo.Migrations.AddPostsTable do
   use Ecto.Migration
 
   def change do
+    # should the argument be an atom or a string? docs has a string
     create table(:posts) do
-      add :title, :string, size: 50
-      add :body, :text
+      add :title,   :string, size: 50
+      add :body,    :text
+
       timestamps()
     end
   end
